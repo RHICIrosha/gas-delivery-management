a6f35e03a2662c27b9f1238c2bbe3e10944cd01b
Irosha <ichathumini4@gmail.com>
2025-02-09 11:32:23 +0530

    irosha new update

--- backend-app/routes/web.php
+++ backend-app/routes/web.php
@@ -1,33 +1,15 @@
-
-
-
-
 <?php
 use app\https\controllers\CustomerOrderController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth\log;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Authentication Routes
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-route::get('/login', [UserController::class,'index'])->name('user.index');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
@@ -60,4 +42,3 @@ Route::middleware('auth')->group(function () {
         ->name('business.dashboard')
         ->middleware('role:business');
 });
-
